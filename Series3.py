3608d419282b3ee5440c6f928aa8cf6105202881
Klinton Chakma <159724195+klinton404@users.noreply.github.com>
2025-08-02 19:17:56 +0600

    Pandas ( Upgrade version of Series3 )

--- Series3.py
+++ Series3.py
@@ -15,3 +15,6 @@ print("\n First 5 Element: \n", s.head())
 print("\n First 2 Element: \n", s.head(2))
 print("\n Last 5 Element: \n", s.tail())
 print("\n Last 2 Element: \n", s.tail(2))
+
+
+print("\n Series Information: \n", s.info())
